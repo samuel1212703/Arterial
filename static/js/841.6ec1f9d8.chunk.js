a6f456cf6f9daa9dab769ca4543045662f5c846d
samuel1212703 <37654743+samuel1212703@users.noreply.github.com>
2023-04-25 14:58:34 +0200

    Updates

---
+++ static/js/841.6ec1f9d8.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 841.6ec1f9d8.chunk.js.LICENSE.txt */
+"use strict";(self.webpackChunkarterial=self.webpackChunkarterial||[]).push([[841],{841:(e,t,r)=>{r.r(t),r.d(t,{startStatusTap:()=>o});var n=r(554),s=r(344),a=r(811);const o=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,n.wj)((()=>{const t=e.innerWidth,r=e.innerHeight,o=document.elementFromPoint(t/2,r/2);if(!o)return;const i=(0,s.a)(o);i&&new Promise((e=>(0,a.c)(i,e))).then((()=>{(0,n.Iu)((async()=>{i.style.setProperty("--overflow","hidden"),await(0,s.s)(i,300),i.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=841.6ec1f9d8.chunk.js.map
